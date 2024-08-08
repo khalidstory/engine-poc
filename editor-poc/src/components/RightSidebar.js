e1c1e5069864fdee6e6c2c61dddc64d9d1a15f7e
abdelaziz-noaman <165668662+Abdelaziz-Noaman@users.noreply.github.com>
2024-08-08 19:39:28 +0300

    Implemented generic function to handle passing elements with nested components, and changes in addItem function

--- editor-poc/src/components/RightSidebar.js
+++ editor-poc/src/components/RightSidebar.js
@@ -4,44 +4,98 @@ import DropItem from "./DropItem";
 
 const RightSidebar = ({ items, setItems }) => {
   const [, drop] = useDrop({
-    accept: ["input", "textarea", "text", "section", "video"],
+    accept: ["input", "textarea", "text", "section", "video", "source", "div"],
     drop: (item) => {
       let itemContent = "";
+      let itemAttributes = {};
       if (item.type === "text") {
         itemContent = prompt("Please add item content") || "";
+        console.log("itemContent:::", typeof itemContent);
       }
       if (item.type === "video") {
+        itemAttributes = {
+          width: "100%",
+          height: "200px",
+          autoplay: true,
+          muted: true,
+        };
         itemContent = (
           <source
             src={prompt("Please add your video source") || ""}
             type="video/mp4"
           />
         );
+
+        console.log("itemContent:::", itemContent);
       }
-      addItem(item.type, item.tagName, itemContent);
+
+      // if (itemContent.type === "source") {
+      //   itemAttributes = {
+      //     type: "video/mp4",
+      //   };
+      // }
+      addItem(item.type, item.tagName, itemContent, itemAttributes);
     },
   });
 
-  const addItem = (type, tagName, itemContent) => {
-    setItems([
-      ...items,
-      {
-        id: items.length,
-        type: type,
-        tagName: tagName,
-        attributes: {
-          class: "item",
-          width: type === "video" && "100%",
-          height: type === "video" && "200px",
-          autoplay: type === "video" && true,
-          muted: type === "video" && true,
-        },
-        content: itemContent,
-        styles: {
-          minWidth: "100px",
-        },
+  const addItem = (type, tagName, itemContent, itemAttributes) => {
+    const newItem = {
+      id: items.length,
+      type: type,
+      tagName: tagName,
+      attributes: {
+        class: "item",
+        ...itemAttributes,
       },
-    ]);
+      content: itemContent,
+      styles: {
+        minWidth: "100px",
+      },
+      components: handleComponents(itemContent),
+    };
+    console.log("🚀 ~ addItem ~ newItem:", newItem);
+
+    function handleComponents(itemContent) {
+      // if there is no content
+      if (!itemContent) return [];
+
+      let components = [];
+
+      const handleChildren = (child) => {
+        if (React.isValidElement(child)) {
+          const content = child.props.children ?? child.props ?? "";
+          const getAttributes = () => {
+            const { children, ...rest } = child.props;
+            return rest;
+          };
+
+          const childItem = addItem(
+            child.type,
+            child.type,
+            content,
+            getAttributes()
+          );
+
+          components.push(childItem);
+        }
+      };
+
+      // if it's a single child element
+      if (React.isValidElement(itemContent)) {
+        handleChildren(itemContent);
+      }
+
+      // if it's multi children elements
+      if (Array.isArray(itemContent)) {
+        itemContent.forEach((item) => handleChildren(item));
+      }
+
+      return components;
+    }
+
+    setItems([...items, newItem]);
+
+    return newItem; // Return the newly created item
   };
 
   return (
